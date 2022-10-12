40691f2835c98a5617abd9272153e9ce917ae78d
Serhii-Filatov <filser.spirit@gmail.com>
2022-10-12 15:52:00 +0300

    Update prettier

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,23 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 
 const refs = {
-  gallery: document.querySelector('.gallery'),
-};
+    gallery: document.querySelector('.gallery'),
+}
 
-const markupArr = galleryItems.map(item => {
-  return `<a class="gallery__item" href="${item.original}">
+const markupArr = galleryItems.map((item) => {
+    return `<a class="gallery__item" href="${item.original}">
                 <img class="gallery__image" src="${item.preview}" 
                         alt="${item.description}"
                         title="${item.description}"/>
             </a>`;
-});
+})
 
-refs.gallery.insertAdjacentHTML('afterbegin', markupArr.join(''));
+refs.gallery.insertAdjacentHTML('afterbegin', markupArr.join(''))
 
-refs.gallery.addEventListener('click', zoomImage);
+refs.gallery.addEventListener('click', zoomImage)
 
-const lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250 });
+const lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250});
 
 function zoomImage(event) {
-  event.preventDefault();
+    event.preventDefault();  
 }
