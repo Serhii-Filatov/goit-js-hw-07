b861674017f2ea189fe68d5c3ffc2bed08cd4306
Serhii-Filatov <filser.spirit@gmail.com>
2022-10-12 15:36:48 +0300

    Обновление проекта

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,55 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  gallery: document.querySelector('.gallery'),
+  body: document.querySelector('body'),
+};
+
+const markupArr = galleryItems.map(item => {
+  return `<div class="gallery__item">
+          <a class="gallery__link" href="${item.original}">
+              <img
+              class="gallery__image"
+              src="${item.preview}"
+              data-source="${item.original}"            
+              alt="${item.description}"
+              />
+          </a>
+      </div>`;
+});
+
+refs.gallery.insertAdjacentHTML('afterbegin', markupArr.join(''));
+
+refs.gallery.addEventListener('click', zoomImage);
+
+let instance = {};
+
+function zoomImage(event) {
+  event.preventDefault();
+
+  instance = basicLightbox.create(
+    `      
+              <img src="${event.target.getAttribute('data-source')}"
+                  alt="${event.target.getAttribute('alt')}"
+              />`,
+    {
+      onClose: instance => {
+        console.log('like it');
+        window.removeEventListener('keydown', onPressEscBtn);
+        refs.body.style.overflow = '';
+      },
+    }
+  );
+
+  instance.show();
+
+  refs.body.style.overflow = 'hidden';
+
+  document.addEventListener('keydown', onPressEscBtn);
+}
+
+function onPressEscBtn(event) {
+  if (event.code === 'Escape') {
+    instance.close();
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  gallery: document.querySelector('.gallery'),
+};
+
+const markupArr = galleryItems.map(item => {
+  return `<a class="gallery__item" href="${item.original}">
+                <img class="gallery__image" src="${item.preview}" 
+                        alt="${item.description}"
+                        title="${item.description}"/>
+            </a>`;
+});
+
+refs.gallery.insertAdjacentHTML('afterbegin', markupArr.join(''));
+
+refs.gallery.addEventListener('click', zoomImage);
+
+const lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250 });
+
+function zoomImage(event) {
+  event.preventDefault();
+}
